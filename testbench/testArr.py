b304f53c2aa8114ffea3f8c617d736fe99cabd00
DotPonder <dotponder@outlook.com>
2022-05-23 20:40:22 +0800

    test arr[arr[]]

---
+++ testbench/testArr.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a0 = [0, 0, 0, 0, 0, 0, 0, 0]
+    a1 = [1, 1, 1, 1, 1, 1, 1, 1]
+    a2 = [2, 2, 2, 2, 2, 2, 2, 2]
+    arr = []
+    arr.append(a1)
+    arr.append(a2)
+    arr.append(a0)
+
